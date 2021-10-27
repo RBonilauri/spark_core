9e3455f84898ca0ece7ba86c0c025603971b8382
Rafael <rafael.bonilauri@wanadoo.fr>
2021-10-27 20:17:41 +0200

    add SparkSession

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,15 @@
+import org.apache.spark.sql.SparkSession
+
 object Main {
 
   def main(args: Array[String]): Unit = {
 
     println("Hello world !")
 
+    val spark = SparkSession
+      .builder()
+      .appName("SparkProject")
+      .master(master = "local[*]")
+      .getOrCreate()
   }
-
-
-  }
+}
